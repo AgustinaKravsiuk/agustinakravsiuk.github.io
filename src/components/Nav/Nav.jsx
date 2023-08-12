d047eeb6205831ae43c54cd26a2dfbd10c825064
AgustinaKravsiuk <agustinakravsiuk@gmail.com>
2023-08-12 17:03:31 -0300

    first commit

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from 'react-router-dom';
+import styles from './Nav.module.css'
+
+const Nav = () => {
+  return (
+    <div className={styles.Nav}>
+      <NavLink></NavLink>
+    </div>
+  )
+};
+
+export default Nav
